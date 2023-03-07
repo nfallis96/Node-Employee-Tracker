150741e310567069c680952b004cce3241e279bb
nfallis96 <nfallis2019@fau.edu>
2023-03-07 06:58:44 -0500

    starting the index.js file

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// required material
+// modules needed
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 require('dotenv').config();
 
-// links for sql
+// to link the SQL database
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "employeeTracker_db"
+    password: "rootroot",
+    database: "etracker_db"
 });
 
-db.connect(function(err) {
+db.connect(function (err) {
     if (err) throw err
     console.log("MySQL Connected")
-    startPrompt();
+    show_input();
 });
+
