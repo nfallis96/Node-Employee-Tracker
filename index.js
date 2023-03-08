00b32bb74667ae592ec60676f1a6d144b2c0e722
nfallis96 <nfallis2019@fau.edu>
2023-03-08 14:08:15 -0500

    startprompt and view all departments section js file

--- index.js
+++ index.js
@@ -15,6 +15,56 @@ const db = mysql.createConnection({
 db.connect(function (err) {
     if (err) throw err
     console.log("MySQL Connected")
-    show_input();
+    startPrompt();
 });
 
+// For startPrompt function 
+const startPrompt = () => {
+    return inquirer.prompt([{
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+    }]).then(function (val) {
+        switch (val.action) {
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+
+            case "View All Roles":
+                viewAllRoles();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Add Department":
+                addDepartment();
+                break;
+
+            case "Add Role":
+                addRole();
+                break;
+
+
+            case "Update Employee Role":
+                updateEmployee();
+        }
+    });
+}
+
+// view all departments============================================
+const viewAllDepartments = () => {
+    const query = `SELECT * FROM departments`;
+    db.query(query,
+        function(err, res) {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+};
